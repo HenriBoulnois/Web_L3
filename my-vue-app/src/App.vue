d9d0ff14b7fb4e4af5348467aa7d7a36e36ff9ac
root <root@Yume-Pc.localdomain>
2021-02-09 13:07:53 +0100

    Correction

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <SelectArray />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SelectArray from './components/Exo2/SelectArray.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    SelectArray,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
